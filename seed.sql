126287a646dcd43b83d822f70f1636e57a156730
Ian Pyeatt <pyeatt08@gmail.com>
2020-09-30 09:23:32 -0500

    updated seed.sql

--- seed.sql
+++ seed.sql
@@ -1,30 +1,43 @@
-drop database if exists employeeCMS_db;
+drop database if exists employeeCMS_DB;
+create database employeeCMS_DB;
+use employeeCMS_DB;
 
-create database employeeCMS_db;
+create table Departments (
+ id INT NOT NULL AUTO_INCREMENT,
+ department_name varchar (20) ,
+ primary key (id)
+ );
+ 
+ insert into Departments (department_name)
+ values ( "engineering");
+ 
+ 
+ create table Employees (
+ id INT NOT NULL AUTO_INCREMENT,
+ first_name varchar (20) ,
+ last_name varchar (20) ,
+ role_id int ,
+ manager_id int,
+ primary key (id)
+ );
+ 
+  insert into Employees (first_name,last_name,manager_id)
+ values ("Ian" , "Pyeatt", 1);
+  create table employee_role (
+  id int not null auto_increment,
+  title varchar (30),
+  salary decimal,
+  departmentId int ,
+  primary key(id));
+  
+    insert into employee_role (title, salary)
+ values ("Full Stack Developer" , 100000.00);
 
-use employeeCMS_db;
 
-create table department (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(30),
-    primary key (id)
-);
+-- joinning the id of the department from the employee_role and department Tables
+ use employeeCMS_DB;
 
-CREATE TABLE role (
-    id INT AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
-    primary key (id),
-    
-);
 
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-    primary key (id),
-    
-); 
+
+select departments.id from employee_role
+inner join departments on employee_role.departmentId = departments.ID
